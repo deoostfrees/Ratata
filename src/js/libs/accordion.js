5c5d8e2a4be0cae754b395d46e4e2bf21bc88063
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2024-02-09 21:30:06 +0100

    [TASK] Prepare accordions

---
+++ src/js/libs/accordion.js
@@ -0,0 +1,3 @@
+export default function accordion () {
+  console.log('TODO: Accordion')
+}
---
+++ src/js/ratata.js
@@ -0,0 +1,5 @@
+if (document.querySelector('.accordion') !== null) {
+  import('./libs/accordion.js').then(({ default: accordion }) => {
+    accordion()
+  })
+}

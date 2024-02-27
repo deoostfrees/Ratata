7a0ddce668f70eb84b04d9006f8251a94550bf96
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2024-02-27 10:12:09 +0100

    [TASK] Improve readability

--- src/js/ratata.js
+++ src/js/ratata.js
@@ -2,7 +2,8 @@
  * Accordion
  */
 if (document.querySelector('.accordion') !== null) {
-  import('./libs/accordion.js').then(({ default: accordion }) => {
-    accordion()
-  })
+  import('./libs/accordion.js')
+    .then(({ default: accordion }) => {
+      accordion()
+    })
 }

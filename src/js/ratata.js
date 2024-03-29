c7c9cff7cc8beb5f4d4e277838e0683d50959460
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2024-03-29 08:59:44 +0100

    [TASK] Prepare theme switcher, rename CSS file, move JavaScript files (WIP)

--- src/js/ratata.js
+++ src/js/ratata.js
@@ -1,8 +1,18 @@
 /**
+ * Theme switch
+ */
+if (document.querySelector('.btn--theme-switch') !== null) {
+  import('./libs/_theme-switch.js')
+    .then(({ default: themeSwitch }) => {
+      themeSwitch()
+    })
+}
+
+/**
  * Accordion
  */
 if (document.querySelector('.accordion') !== null) {
-  import('./libs/accordion.js')
+  import('./libs/_accordion.js')
     .then(({ default: accordion }) => {
       accordion()
     })

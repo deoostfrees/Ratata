8886e1a50680f82191920d9667f07594c5a916d8
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2024-04-17 18:31:42 +0200

    [TASK] Add comments

--- src/js/set-theme.js
+++ src/js/set-theme.js
@@ -1,3 +1,7 @@
+/**
+ * Load this file in the <head> to avoid FART (Flash of inAccurate coloR Theme)
+ *
+ */
 if (localStorage.getItem('color-mode')) {
   document.documentElement.setAttribute('data-theme', localStorage.getItem('color-mode'))
 }

480ecb32065b9480d0121cb9dcc47d0dea718b4c
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2024-04-20 18:30:44 +0200

    [TASK] Rename/ Move theme switcher files/ functions

--- src/js/set-theme.js
+++
@@ -1,7 +0,0 @@
-/**
- * Load this file in the <head> to avoid FART (Flash of inAccurate coloR Theme)
- *
- */
-if (localStorage.getItem('color-mode')) {
-  document.documentElement.setAttribute('data-theme', localStorage.getItem('color-mode'))
-}

40cb5f9f9662ee46f9a3d8bdf9ca9bf32bcad110
deoostfrees <68224180+deoostfrees@users.noreply.github.com>
2025-10-17 16:47:27 +0200

    [TASK] Use `requestIdleCallback()` to load low-priority modules

--- src/js/ratata.js
+++ src/js/ratata.js
@@ -1,21 +1,44 @@
 /**
- * Toggle color scheme
+ * Load low priority modules
  *
+ * @param {object} elements Object containing the DOM elements
  */
-if (document.querySelector('.btn--color-scheme-switch') !== null) {
-  import('./libs/_color-scheme-toggle.js')
-    .then(({ default: colorSchemeToggle }) => {
-      colorSchemeToggle()
-    })
+function loadLowPriorityModules (elements) {
+  // Accordion
+  if (elements.accordion !== null) {
+    import('./libs/_accordion.js')
+      .then(({ default: accordion }) => {
+        accordion()
+      })
+  }
 }
 
 /**
- * Accordion
+ * Load high priority modules
  *
+ * @param {object} elements Object containing the DOM elements
  */
-if (document.querySelector('.accordion') !== null) {
-  import('./libs/_accordion.js')
-    .then(({ default: accordion }) => {
-      accordion()
-    })
+function loadHighPriorityModules (elements) {
+  // Color scheme toggle
+  if (elements.colorSchemeToggle !== null) {
+    import('./libs/_color-scheme-toggle.js')
+      .then(({ default: colorSchemeToggle }) => {
+        colorSchemeToggle()
+      })
+  }
+}
+
+const elements = {
+  colorSchemeToggle: document.querySelector('.btn--color-scheme-switch'),
+  accordion: document.querySelector('.accordion')
+}
+
+loadHighPriorityModules(elements)
+
+if ('requestIdleCallback' in window) {
+  requestIdleCallback(() => loadLowPriorityModules(elements), {
+    timeout: 1000
+  })
+} else {
+  setTimeout(() => loadLowPriorityModules(elements), 100)
 }
